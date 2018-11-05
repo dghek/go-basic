737f98ab048525fe6f27c8a05c73d57f9137a644
dghek <dgherrig@gmail.com>
2018-11-05 13:39:30 -0800

    initial stuff

--- go-basic.go
+++ go-basic.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+    "math"
 )
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+    fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+    
 }
 
 
