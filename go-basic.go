96ba0437a7106386ed133e7d9ebd4024c256630e
dghek <dgherrig@gmail.com>
2018-11-05 15:27:20 -0800

    updates

--- go-basic.go
+++ go-basic.go
@@ -1,9 +1,32 @@
 package main
 
+//   Note that: := is a declaration, whereas = is an assignment
+
+//bool
+//
+//string
+//
+//int  int8  int16  int32  int64
+//uint uint8 uint16 uint32 uint64 uintptr
+//
+//byte // alias for uint8
+//
+//rune // alias for int32
+//     
+//
+//float32 float64
+//
+//complex64 complex128
+
+// pointers
+//  var p *int
+//  x = *p
+
 import (
 	"fmt"
 	"math/rand"
     "math"
+	"math/cmplx"
 )
 
 var i, j int = 1, 2
@@ -24,6 +47,13 @@ func split(sum int) (x, y int) {
 	return
 }
 
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+const Pi = 3.14
+
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
@@ -40,6 +70,28 @@ func main() {
 	var c, python, java = true, false, "no!"
 	fmt.Println(i, j, c, python, java)
 	
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	var f float64
+	var bo bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, bo, s)
+	
+	
+	var x, y int = 3, 4
+	var ff float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(ff)
+	fmt.Println(x, y, z)
+	
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
 }
 
 
